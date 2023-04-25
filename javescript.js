2a1e2215278a39aa8e0050389e74204a8978462e
Alaeddin Shebani <116307373+Alaeddin1990@users.noreply.github.com>
2023-04-25 06:37:25 +0200

    Porsche-web

---
+++ javescript.js
@@ -0,0 +1,45 @@
+
+
+let sssss = document.querySelector(".H123")
+
+
+window.onscroll = function one(){
+
+    if (window.scrollY >= 300)   {
+        
+        
+    sssss.style.display="block"
+    
+    }else{sssss.style.display="none"}
+}
+
+
+let ssss = document.querySelector(".H1234")
+
+
+sssss.onclick = function two(){
+    if (ssss.style.display ==="block") {
+    ssss.style.display="none";
+    
+}else{ssss.style.display=" block";}
+}
+sssss.addEventListener("scroll","click",one,two)
+
+
+
+
+
+
+
+    
+
+
+
+
+
+// () {} [] 
+
+
+
+
+
